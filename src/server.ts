b7aa7f83409d59cbb76d632d85db312db61b4cbd
Roody <rjointer2@gmail.com>
2022-03-31 11:23:27 -0700

    now we can have the derivations of the x and y positions and we can now start collison physics on the server to emit to the client

--- src/server.ts
+++ src/server.ts
@@ -16,14 +16,14 @@ const io = require('socket.io')(server, {
 });
 
 let clients: {[index: string]: { 
-    x: number, y: number, host: boolean, height: number, width: number 
+    x: number, y: number, host: boolean, height: number, width: number,  dx: number, dy: number
 }} = {};
 
 
 io.on('connection', ( socket: Socket ) => {
 
     if( !clients['egg'] ) {
-        clients['egg'] = { x: 320, y: 240, height: 20, width: 20, host: false, }
+        clients['egg'] = { x: 320, y: 240, height: 20, width: 20, host: false, dx: 0, dy: 0 }
     }
 
     setInterval(() => {
@@ -31,19 +31,31 @@ io.on('connection', ( socket: Socket ) => {
         let currentObjs = Object.keys(clients)
 
         if(clients['egg']) {
+
+            const prevX = clients['egg'].x
+            const prevY = clients['egg'].y
+
+            const newX = prevX+1;
+            const newY = clients['egg'].y;
+
+            clients['egg'].x = newX
+
             //clients['egg'].x++;
+
+            clients['egg'].dx = prevX - newX
+            clients['egg'].dy = prevY - newY
 
             if( clients[ currentObjs[ 1 ] ] ) {
                 if( clients[ currentObjs[1] ].x - clients['egg'].x > -20 
                     && clients[ currentObjs[1] ].x - clients['egg'].x < 20 
                         && clients[ currentObjs[1] ].y - clients['egg'].y > -20 
                             && clients[ currentObjs[1] ].y - clients['egg'].y < 20  ) {
-                    console.log('touching')
+                    //console.log('touching')
                 }
             }
 
-            if( clients['egg'].y > 450 ) {
-                clients['egg'].y = 450
+            if( clients['egg'].y > 470 ) {
+                clients['egg'].y = 470
             }
             if( clients['egg'].x < 10 ) {
                 clients['egg'].x = 10
@@ -51,8 +63,8 @@ io.on('connection', ( socket: Socket ) => {
             if( clients['egg'].y < 10 ) {
                 clients['egg'].y = 10
             }
-            if( clients['egg'].x > 610 ) {
-                clients['egg'].x = 610
+            if( clients['egg'].x > 630 ) {
+                clients['egg'].x = 630
             }
 
             io.emit('position', clients)
@@ -60,17 +72,31 @@ io.on('connection', ( socket: Socket ) => {
     }, 1000/60)
     
 
-    socket.on("move", ( res: { id: string, x: number, y: number, host: boolean }) => {
+    socket.on("move", ( res: { id: string, x: number, y: number, host: boolean, dx: number, dy: number }) => {
+
 
         if(clients[res.id]) {
-            if( clients[res.id] ) {
-                clients[res.id].x = res.x
-                clients[res.id].y = res.y
-            }
+
+            const prevX = clients[res.id].x
+            const prevY = clients[res.id].y
+
+            const newX = res.x;
+            const newY = res.y;
+
+            clients[res.id].x = newX
+            clients[res.id].y = newY
+            clients[res.id].dx = prevX - newX
+            clients[res.id].dy = prevY - newY
+            //prevX - newX > 0 ? console.log('left') : console.log('right');
+            //prevY - newY > 0 ? console.log('up') : console.log('down');
+
+            console.log(`x_vel: ${prevX - newX}, y_vel: ${prevY - newY}`)
+
+
             
             // wall detction 
-            if( clients[res.id].y > 450 ) {
-                clients[res.id].y = 450
+            if( clients[res.id].y > 470 ) {
+                clients[res.id].y = 470
             }
             if( clients[res.id].x < 10 ) {
                 clients[res.id].x = 10
@@ -78,9 +104,14 @@ io.on('connection', ( socket: Socket ) => {
             if( clients[res.id].y < 10 ) {
                 clients[res.id].y = 10
             }
-            if( clients[res.id].x > 610 ) {
-                clients[res.id].x = 610
+            if( clients[res.id].x > 630 ) {
+                clients[res.id].x = 630
             }
+
+            console.log( 
+                clients[res.id].x, 
+                clients[res.id].y, 
+            )
         }
 
 
@@ -92,9 +123,7 @@ io.on('connection', ( socket: Socket ) => {
         clients[socket.id] = clientData;
         socket.emit('registerId', socket.id)
 
-        if(Object.keys(clients).length === 2) {
-            clients[ Object.keys(clients)[1]].host = true
-        }
+
 
         //console.log(clients)
         io.emit('position', clients);
@@ -102,9 +131,6 @@ io.on('connection', ( socket: Socket ) => {
 
     socket.on('disconnect', () => {
 
-        if(clients[socket.id].host ) {
-            clients[ Object.keys(clients)[1] ].host = true
-        }
 
         delete clients[socket.id];
 
@@ -123,46 +149,7 @@ server.listen(_port, () => {
     console.log(`server listening`)
 })
 
-
 /* 
-
-
-
-socket.on("move", ( res: { id: string, keyCode: number, type: string } ) => {
-
-        console.log(res)
-        
-        // key state 
-        let left = false;
-        let up = false;
-        let right = false;
-        let down = false;
-
-        let keyState = res.type === "keydown" ? true : false;
-
-        if( res.keyCode === 37 ) {
-            left = keyState;
-            clients[res.id].x--
-        }
-        if( res.keyCode === 38 ) {
-            up = keyState;
-            clients[res.id].y--
-        }
-        if( res.keyCode === 39 ) {
-            right = keyState;
-            clients[res.id].x++
-        }
-        if( res.keyCode === 40 ) {
-            down = keyState;
-            clients[res.id].y++
-        }
-        
-
-
-        io.emit('position', clients);
-
-    })
-
 
 
 
