915af7c272b7117caeeaaeade54eeca7b17bee2a
Roody <rjointer2@gmail.com>
2022-04-04 19:03:13 -0700

    added theat the gae is base onkeyboard events to move instead

--- src/game_engine/socket_methods/receiveUserInputs.ts
+++ src/game_engine/socket_methods/receiveUserInputs.ts
@@ -5,7 +5,12 @@ import clients from "../clients";
 export default function receiveUserInputs( socket: Socket, io: Socket ) {
 
     socket.on("move", ( res: { 
-        id: string, x: number, y: number, 
+        id: string, 
+        direction: { 
+            ArrowLeft: boolean, ArrowRight: boolean, 
+            ArrowUp: boolean, ArrowDown: boolean,
+            Shift: boolean 
+        }
         host: boolean, dx: number, dy: number,
         speed: number, hold: boolean
         }
@@ -14,7 +19,24 @@ export default function receiveUserInputs( socket: Socket, io: Socket ) {
 
         if(clients[res.id]) {
 
-            const prevX = clients[res.id].x
+            if( !res.direction.Shift ) {
+                if( res.direction.ArrowRight ) clients[res.id].x = clients[res.id].x + 3;
+                if( res.direction.ArrowUp ) clients[res.id].y = clients[res.id].y - 3;
+                if( res.direction.ArrowLeft ) clients[res.id].x = clients[res.id].x - 3;
+                if( res.direction.ArrowDown ) clients[res.id].y = clients[res.id].y + 3;
+                clients[res.id].hold = false
+            }
+            
+            console.log(res.direction);
+
+            if( res.direction.Shift ) clients[res.id].hold = true
+
+
+
+            /* clients[res.id].x = res.x
+            clients[res.id].y = res.y */
+
+            /* const prevX = clients[res.id].x
             const prevY = clients[res.id].y
 
             const newX = res.x;
@@ -23,31 +45,19 @@ export default function receiveUserInputs( socket: Socket, io: Socket ) {
             clients[res.id].x = newX
             clients[res.id].y = newY
             clients[res.id].dx = prevX - newX
-            clients[res.id].dy = prevY - newY
+            clients[res.id].dy = prevY - newY */
+
             //prevX - newX > 0 ? console.log('left') : console.log('right');
             //prevY - newY > 0 ? console.log('up') : console.log('down');
 
             //console.log(`x_vel: ${prevX - newX}, y_vel: ${prevY - newY}`)
-
-            clients[res.id].hold = res.hold
-
-
-            
+        
             // wall detction 
-            if( clients[res.id].y > 470 ) {
-                clients[res.id].y = 470
-            }
-            if( clients[res.id].x < 10 ) {
-                clients[res.id].x = 10
-            }
-            if( clients[res.id].y < 10 ) {
-                clients[res.id].y = 10
-            }
-            if( clients[res.id].x > 630 ) {
-                clients[res.id].x = 630
-            }
+            if( clients[res.id].y > 470 )   clients[res.id].y = 470
+            if( clients[res.id].x < 10 )    clients[res.id].x = 10
+            if( clients[res.id].y < 10 )    clients[res.id].y = 10
+            if( clients[res.id].x > 630 )   clients[res.id].x = 630
 
-            console.log(clients[res.id].hold)
         }
 
 
